90d0b788154088bd84f39307652aba84c942f248
HBsoft <HBsoft@HBsoftdeMacBook-Pro.local>
2021-01-02 15:49:56 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#商铺数据加载并存储
+# 读取文件
+ fileObj = open()
+# 解析数据
+# 数据清晰
+# 保存数据
